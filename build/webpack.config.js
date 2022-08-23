9fc99f87baa7c560985192d0065941dc6fc3cce7
AmdreiMash <100795104+AmdreiMash@users.noreply.github.com>
2022-08-23 17:26:37 +0300

    add: change webpack config

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -3,7 +3,15 @@ const path = require('path');
 module.exports = {
 	entry: './src/index.js',
 	output: {
-		filename: 'main.js',
+		filename: 'builde.js',
 		path: path.resolve(__dirname, 'eldritch-codejam'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+		],
 	},
 };
