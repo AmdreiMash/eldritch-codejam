19f8084edc282e5ecf8ac5aa560b6b0d22be12d1
AmdreiMash <100795104+AmdreiMash@users.noreply.github.com>
2022-08-23 22:36:31 +0300

    sdfv

--- build/src/index.js
+++
@@ -1,9 +0,0 @@
-import _ from 'lodash';
-import './style.css';
-import ancients from '../assets/Ancients/index.js'
-import BGimg from '../assets/home.png'
-
-console.log('Hello!')
-console.log(ancients)
-
-document.querySelector('body').style.backgroundImage = `url('${BGimg}')`
