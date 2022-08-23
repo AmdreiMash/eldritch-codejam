a815de454e152989a749dfefb903fe587d598fbf
AmdreiMash <100795104+AmdreiMash@users.noreply.github.com>
2022-08-24 01:33:55 +0300

    fert: add antcionts img to the page, start perform functiinality

--- build/src/getAntcient.js
+++ build/src/getAntcient.js
@@ -1,11 +1,22 @@
 import { chain } from 'lodash';
 import ancients from '../assets/Ancients/index.js';
 
+
 export function getAntcient() {
 	for (let key in ancients) {
 		let div = document.createElement('div')
 		div.id = key
+		div.classList.add('ancient')
 		div.style.backgroundImage = `url('${ancients[key]}')`
 		document.querySelector('.ancients').append(div)
+		div.addEventListener('click', () => {
+			event.target.classList.add('active')
+			//for (let i = 0; i < document.querySelector('.ancients').childNodes.length; i++) {
+			//};
+			console.log(document.querySelector('.ancients').childNodes)
+		})
 	}
+	//let arr = document.querySelectorAll('.ancient')
+	console.log(arr)
 }
+
--- build/src/index.js
+++ build/src/index.js
@@ -2,9 +2,10 @@ import _ from 'lodash';
 import './style.css';
 import ancients from '../assets/Ancients/index.js';
 import BGimg from '../assets/home.png';
-import { getAntcient } from './getAntcient.js';
+import { getAntcient, getChoice } from './getAntcient.js';
 
 console.log(ancients)
 document.body.style.backgroundImage = `url('${BGimg}')`
 
+const ancientsW = document.querySelector('.ancients')
 getAntcient()
